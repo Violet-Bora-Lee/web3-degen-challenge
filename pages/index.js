696c50761a19476fed74d40b8d1cead09fd7f33a
Bora Lee <learnerbr@gmail.com>
2023-09-02 03:09:54 +0900

    fix lint error

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Switch from "react-switch";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -72,11 +73,11 @@ export default function Home() {
             Make a habit to explore Web3 ecosystem together
           </h1>
           <div class="mt-10 flex justify-center gap-x-6">
-            <a
+            <Link
               className="group inline-flex items-center justify-center rounded-full
                py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-dark-yellow hover:text-slate-100 active:bg-slate-800 active:text-slate-300
                 focus-visible:outline-slate-900"
-              href="/dashboard">Start To Be A Web3 Degen</a>
+              href="/dashboard">Start To Be A Web3 Degen</Link>
           </div>
         </div>
       
