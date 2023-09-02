8139f6f18384b7d7c5f10ea2ed76d704daf8a37b
Bora Lee <learnerbr@gmail.com>
2023-09-02 22:54:13 +0900

    work for UI(WIP)

--- pages/challenge1.js
+++ pages/challenge1.js
@@ -55,6 +55,41 @@ export default function Challenge1() {
   const [darkMode, setDarkMode] = useState(true);
   const [selectedPlans, setSelectedPlans] = useState([]);
   const [loadingPlan, setLoadingPlan] = useState(null);
+  const [showModal, setShowModal] = useState(true);
+  
+  function Modal({ onClose }) {
+    return (
+      <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75" aria-hidden="true"></div>
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+          
+          <div className="inline-block align-bottom bg-white dark:bg-gray-900 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="bg-white dark:bg-gray-900 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                Plan Selected!
+              </h3>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  Your plan has been successfully selected.
+                </p>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={onClose}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
   
   const togglePlan = (planName) => {
     if (selectedPlans.includes(planName)) return;
@@ -67,7 +102,9 @@ export default function Challenge1() {
         if (prev.includes(planName)) {
           return prev.filter(p => p !== planName);
         } else {
-          return [...prev, planName];
+          const newPlans = [...prev, planName];
+          setShowModal(true); // 모달을 활성화
+          return newPlans;
         }
       });
       
@@ -205,6 +242,7 @@ export default function Challenge1() {
           </section>
         </div>
       </main>
+      {/*{showModal && <Modal onClose={() => setShowModal(false)} />}*/}
     </div>
   )
     ;
