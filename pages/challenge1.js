5493dd42c5d3442264f43c9107287f090565a45c
Bora Lee <learnerbr@gmail.com>
2023-09-02 19:33:54 +0900

    work for UI

--- pages/challenge1.js
+++ pages/challenge1.js
@@ -5,8 +5,76 @@ import router from "next/router";
 
 import "react-toastify/dist/ReactToastify.css";
 
+import { RadioGroup, Listbox } from "@headlessui/react";
+
+const plans = [
+  {
+    name: "Hobby",
+    price: "$40"
+  },
+  {
+    name: "Startup",
+    price: "$80"
+  },
+  {
+    name: "Business",
+    price: "$160"
+  },
+  {
+    name: "Enterprise",
+    price: "$240"
+  }
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Spinner = () => (
+  <svg
+    className="animate-spin -ml-1 h-5 w-5 text-gray-500"
+    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <circle
+      className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+      strokeWidth="4"></circle>
+    <path
+      className="opacity-75" fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+  </svg>
+);
+
+export const CheckedIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="10" fill="#34D399"/> {/* 초록색 원 */}
+    <path d="M7 12L10 15L17 8" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/> {/* 브이자 */}
+  </svg>
+);
+
+
 export default function Challenge1() {
   const [darkMode, setDarkMode] = useState(true);
+  const [selectedPlans, setSelectedPlans] = useState([]);
+  const [loadingPlan, setLoadingPlan] = useState(null);
+  
+  const togglePlan = (planName) => {
+    if (selectedPlans.includes(planName)) return;
+    // 클릭된 plan의 로딩 상태 설정
+    setLoadingPlan(planName);
+    
+    setTimeout(() => {
+      // 실제 plan 토글 작업 수행
+      setSelectedPlans(prev => {
+        if (prev.includes(planName)) {
+          return prev.filter(p => p !== planName);
+        } else {
+          return [...prev, planName];
+        }
+      });
+      
+      // 로딩 상태 초기화
+      setLoadingPlan(null);
+    }, 1000);
+  };
   
   useEffect(() => {
     const body = document.querySelector("body");
@@ -16,17 +84,17 @@ export default function Challenge1() {
   const files = [
     {
       source:
-        'linea-challenge.png',
+        "linea-challenge.png"
     },
     {
       source:
-        'optimism-challenge.png',
+        "optimism-challenge.png"
     },
     {
       source:
-        'taiko-challenge.png',
-    },
-  ]
+        "taiko-challenge.png"
+    }
+  ];
   
   return (
     <div className="container mx-auto px-4 flex flex-col min-h-screen">
@@ -79,14 +147,65 @@ export default function Challenge1() {
       </header>
       
       <main>
-        <div className="mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div
+          className="mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          {/*상단*/}
           <section className="mt-16">
-            <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg">
-              <img src="/linear-challenge-details.png" alt="" className="pointer-events-none object-cover" />
+            <div
+              className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg">
+              <img src="/linear-challenge-details.png" alt=""
+                   className="pointer-events-none object-cover" />
             </div>
+          </section>
+          {/*하단*/}
+          <section className="mt-16 mb-40">
+            <Listbox as="div" className="space-y-4">
+              {plans.map((plan) => (
+                <Listbox.Option
+                  key={plan.name}
+                  onClick={() => togglePlan(plan.name)}
+                  className={classNames(
+                    selectedPlans.includes(plan.name) ?
+                      "border-indigo-600 ring-2 ring-indigo-600" :
+                      "border-gray-300",
+                    "relative block cursor-pointer rounded-lg border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between"
+                  )}
+                >
+            <span className="flex items-center w-full">
+              <span className="flex flex-row justify-between text-sm w-full">
+                <Listbox.Label as="span" className="font-medium">
+                  {plan.name}
+                </Listbox.Label>
+                {loadingPlan === plan.name ? (
+                  <span className="text-gray-500">
+                    <Spinner />
+                  </span>
+                ) : selectedPlans.includes(plan.name) ? (
+                  <span className="text-green-500 dark:text-green-500!">
+                    <CheckedIcon />
+                  </span>
+                ) : (
+                  <span className="text-gray-500">
+                    Check Availability
+                  </span>
+                )}
+              </span>
+            </span>
+                  <span
+                    className={classNames(
+                      selectedPlans.includes(plan.name) ? "border" : "border-2",
+                      "border-indigo-600",
+                      "pointer-events-none absolute -inset-px rounded-lg"
+                    )}
+                    aria-hidden="true"
+                  />
+                </Listbox.Option>
+              ))}
+            </Listbox>
           </section>
         </div>
       </main>
     </div>
-  );
+  )
+    ;
 }
